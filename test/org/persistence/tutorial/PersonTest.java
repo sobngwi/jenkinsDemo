f7cf66c90d13535d587dcf49d49f9c1f89e57bcb
Alain Narcisse SOBNGWI <sobngwi@gmail.com>
2015-06-28 22:01:03 +0200

    add testObjectReference to see test coverage change.

--- test/org/persistence/tutorial/PersonTest.java
+++ test/org/persistence/tutorial/PersonTest.java
@@ -1,5 +1,11 @@
 package org.persistence.tutorial;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class PersonTest extends TestCase {
@@ -29,5 +35,30 @@ public class PersonTest extends TestCase {
 		String testString = "Fred Flintstone (7 books)";
 		assertEquals(testString, p1.toString());
 	}
+	
+
+	public void testObjectReference() {
+		int a = 3;
+		int b = a;
+		int c = 0 ;
+		a = 4;
+		System.out.println(b);
+		Person p1 = new Person();
+		p1.setName("William Shakespeare");
+		Person p2 = p1;
+		p2.setName("Charles Dickens");
+		assertTrue("What happened to p1's name?",
+				p1.getName().equals(p2.getName()));
+		
+		String s1 = "a string";
+		String s2 = "a string";
+		assertTrue(s1 == s2);
+		s2 = "a string with more";
+		s2 = s2.substring(0,8);
+		assertFalse(s1 == s2);
+		assertTrue(s1.equals(s2));
+		
+	}
+
 
 }
